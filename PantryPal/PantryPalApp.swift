b2264d451d5864bd8d107d38cac5bd4c4fffeb00
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-03-31 12:34:45 -0400

    Initial Commit

---
+++ PantryPal/PantryPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryPalApp.swift
+//  PantryPal
+//
+//  Created by Jibryll Brinkley on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct PantryPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
