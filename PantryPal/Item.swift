8072c5a78ce2aba680c4213849285ac26675a93d
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-03-31 12:56:10 -0400

    create data model + setup model container  & model context + create empty state screen

---
+++ PantryPal/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  PantryPal
+//
+//  Created by Jibryll Brinkley on 3/31/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Item {
+    var title: String
+    var isCompleted: Bool
+    
+    init(title: String, isCompleted: Bool) {
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+}
